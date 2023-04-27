42342f984905b928d0d8c329c5189de3b1ac9b10
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-04-26 20:39:11 -0600

    admin section

--- app.py
+++ app.py
@@ -4,6 +4,22 @@ import pandas as pd
 import plotly.graph_objects as go
 import os
 import mariadb
+from flask import (
+    Flask,
+    render_template,
+    request,
+    json,
+    flash,
+    session,
+    redirect,
+    url_for,
+)
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import dash_bootstrap_components as dbc
+import time
+from flask import Flask, render_template
+from dash import ctx
 
 # Font Awesome Icon's
 external_scripts = [{'src': 'https://kit.fontawesome.com/19f1c21c33.js',
@@ -20,6 +36,9 @@ app = Dash(__name__,
            external_scripts = external_scripts,
            external_stylesheets=external_stylesheets
            )
+
+# Secret key
+app.server.secret_key = "purpleMap"
 
 app.index_string = """<!DOCTYPE html>
 <html>
@@ -50,13 +69,28 @@ app.index_string = """<!DOCTYPE html>
 
 server = app.server
 
-
 # Page layout
-app.layout = html.Div([
+app.layout = html.Div(
+    [
+        # dash.page_container
+        dcc.Location(id="url", refresh=False),
+        html.Div(id="page-content", children=[]),
+    ]
+)
 
-    dash.page_container
+app.config["suppress_callback_exceptions"] = True
 
-])
+# Call to pages
+from pages import (
+    login,
+    index,
+    password_recovery,
+    sent_mail,
+    home,
+    territoria,
+    seccionvioleta,
+    page_not_found,
+)
 
 
 # Navbar - Callback
@@ -510,6 +544,394 @@ app.callback(
     Output("mapa-desktop", "figure"),
     Input("switches-input-desktop", "value")
 )(on_form_change)
+
+
+
+#ACCESING PAGES
+
+# Based on the path, returns a page's layout
+@app.callback(Output("page-content", "children"), Input("url", "pathname"))
+def display_page(pathname):
+    if pathname == "/":
+        return home.layout
+    if pathname == "/territoria":
+        return territoria.layout
+    if pathname == "/seccionvioleta":
+        return seccionvioleta.layout
+    if pathname == "/login":
+        if "user" in session:
+            redirect("/index")
+            return index.layout
+        else:
+            return login.layout
+    if pathname == "/password_recovery":
+        return password_recovery.layout
+    if pathname == "/index":
+        if "user" not in session:
+            return login.layout
+        else:
+            return index.layout
+    else:
+        return page_not_found.layout
+
+#Deleting records route
+@app.server.route("/delete_record/<int:id>",methods=['GET'])
+def page_delete_record(id):
+        if 'user' not in session:
+            return redirect('/page_not_found')
+        else:
+            cur.execute("CALL check_record(%s)",(id,))
+            data=cur.fetchone()
+            if data[0]:
+                cur.execute('CALL get_record(%s)', (id,))
+                table=cur.fetchone()
+                return render_template('delete.html', record=table)
+            else:
+                return redirect('/page_not_found')
+
+#When cancelling a records delete request, redirects to index
+@app.server.route('/cancel')
+def cancel_delete_record():
+    if 'user' not in session:
+            return redirect('/page_not_found')
+    else:
+       return redirect('/index')
+    
+#Delete record from the db
+@app.server.route('/delete/<int:id>',methods=['POST'])
+def execute_delete_record(id):
+    if 'user' not in session:
+        return redirect('/page_not_found')
+    else:
+        cur.execute("CALL delete_record(%s)",(id,))
+        conn.commit()
+        return render_template('/delete_alert.html')
+        
+    
+@app.server.route('/restore_password')
+def restore_password():
+    if 'user' not in session:
+        return render_template('/restore_password.html')
+    else:
+        return redirect('/index')
+# LOGIN
+
+# Checks the conditions the email has to fulfill
+def check_email(email):
+    if email is not None and ("@" not in email or "." not in email or "@." in email):
+        return True
+    return False
+
+
+# Checks the conditions the password has to fulfill
+def check_password(password):
+    if password is not None and len(password) <= 0:
+        return True
+    return False
+
+
+# Validate login form inputs : If at least one input is empty, the submit button is disabled
+@app.callback(
+    Output("submit_button", "disabled"),
+    [Input("email", "value"), Input("password", "value")],
+)
+def login_inputs_validation(email, password):
+    if (
+        email is None
+        or password is None
+        or check_email(email)
+        or check_password(password)
+    ):
+        return True
+    return False
+
+
+# Email input feedback : If the email is not valid, feedback will be shown to the user
+@app.callback(
+    Output("email", "invalid"), Input("email", "value"), prevent_initial_call=True
+)
+def show_email_feedback(email):
+    if check_email(email):
+        return True
+    return False
+
+
+# Password input feedback : If the password is not valid, feedback will be shown to the user
+@app.callback(
+    Output("password", "invalid"), Input("password", "value"), prevent_initial_call=True
+)
+def show_password_feedback(password):
+    if check_password(password):
+        return True
+    return False
+
+
+# User credentials authentication : Checks that both credentials exist in the db to authenticate the user into the system
+@app.callback(
+    [Output("login_form", "children"), Output("bad_credentials_alert", "is_open")],
+    Input("submit_button", "n_clicks"),
+    State("email", "value"),
+    State("password", "value"),
+    prevent_initial_call=True,
+)
+def authenticate_login(n_clicks, email, password):
+    if n_clicks is not None:
+        cur.execute("CALL authenticate_login(%s,%s)", (email, password))
+        data = cur.fetchone()[0]
+        if data:
+            cur.execute("CALL user_num(%s)", (email,))
+            data_id = cur.fetchone()[0]
+            session["user"] = data_id
+            return dcc.Location(href="/index", id="Index"), False
+        else:
+            return True, True
+    return True, False
+
+
+# INDEX (RECORDS)
+
+# Add record modal : Allows the admin to open the modal to add a report
+@app.callback(
+    Output("add_record_modal", "is_open"),
+    Input("open_add_record_modal", "n_clicks"),
+    State("add_record_modal", "is_open"),
+    prevent_initial_call=True,
+)
+def toggle_add_record_modal(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
+
+
+# Checks the conditions the latitude has to fulfill
+def check_latitude(latitude):
+    if latitude is not None:
+        try:
+            number = float(latitude)
+            if number > 90 or number < -90:
+                return False
+            return True
+        except ValueError:
+            return False
+    return False
+
+
+# Checks the conditions the longitude has to fulfill
+def check_longitude(longitude):
+    if longitude is not None:
+        try:
+            number = float(longitude)
+            if number > 180 or number < -180:
+                return False
+            return True
+        except ValueError:
+            return False
+    return False
+
+
+# Validate add record form inputs : If at least one input is empty, the submit button is disabled
+@app.callback(
+    Output("add_record_button", "disabled"),
+    [Input("type", "value"), Input("latitude", "value"), Input("longitude", "value")],
+)
+def add_record_input_validation(type, latitude, longitude):
+    if (
+        check_latitude(latitude) == False
+        or check_longitude(longitude) == False
+        or type is None
+    ):
+        return True
+    return False
+
+
+# Latitude input feedback : If the latitude is not valid, feedback will be shown to the user
+@app.callback(
+    Output("latitude", "invalid"), Input("latitude", "value"), prevent_initial_call=True
+)
+def latitude_validation(latitude):
+    if check_latitude(latitude) == False:
+        return True
+    return False
+
+
+# Longitude input feedback : If the longitude is not valid, feedback will be shown to the user
+@app.callback(
+    Output("longitude", "invalid"),
+    Input("longitude", "value"),
+    prevent_initial_call=True,
+)
+def longitude_validation(longitude):
+    if check_longitude(longitude) == False:
+        return True
+    return False
+
+
+# Filter records modal :  Allows the user to open the modal to filter the records
+@app.callback(
+    Output("filter_records_modal", "is_open", allow_duplicate=True),
+    Input("open_filter_records_modal", "n_clicks"),
+    State("filter_records_modal", "is_open"),
+    prevent_initial_call=True,
+)
+def toggle_filter_records_modal(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
+
+
+# Show records:  Shows the records in the table by default when accesing index but also when using filters
+@app.callback(
+    [Output("table", "children"),Output("filter_records_modal","is_open")],
+    [
+        Input("filter_records_button", "n_clicks"),
+        State("type_filter", "value"),
+        State("day_filter", "value"),
+        State("month_filter", "value"),
+        State("year_filter", "value"),
+    ],
+)
+def show_records(n, type_filter, day_filter, month_filter, year_filter):
+    if n > 0:
+        if type_filter is None:
+            type_filter = ""
+        if day_filter is None:
+            day_filter = ""
+        if month_filter is None:
+            month_filter = ""
+        if year_filter is None:
+            year_filter = ""
+        cur.execute(
+            "CALL filter_records(%s,%s,%s,%s)",
+            (type_filter, day_filter, month_filter, year_filter),
+        )
+        data = cur.fetchall()
+        return create_table(data), False
+    cur.execute("CALL obtain_records()")
+    data = cur.fetchall()
+    return create_table(data), False
+
+
+# Add record form : Inserts the given information as a record in the db and shows succesful alert
+@app.callback(
+    [
+        Output("added_record_alert", "is_open"),
+        Output("add_record_modal", "is_open", allow_duplicate=True),
+    ],
+    Input("add_record_button", "n_clicks"),
+    [State("type", "value"), State("latitude", "value"), State("longitude", "value")],
+    prevent_initial_call=True,
+)
+def add_record(n_clicks, type, latitude, longitude):
+    if n_clicks > 0:
+        cur.execute(
+            "CALL add_record(%s,%s,%s,%s)", (session["user"], type, latitude, longitude)
+        )
+        conn.commit()
+        return True, False
+
+
+# When record added alert is closed, refreshes the page
+@app.callback(
+    Output("url", "pathname", allow_duplicate=True),
+    Input("added_record_alert", "is_open"),
+    prevent_initial_call=True,
+)
+def redirect_after_alert(change):
+    if not change:
+        time.sleep(1)
+        return "/index"
+    raise PreventUpdate
+
+
+# Returns the html table of the given data
+def create_table(data):
+    table_records = []
+    table_headers = html.Tr(
+        [
+            html.Th("id"),
+            html.Th("Tipo"),
+            html.Th("Ubicación"),
+            html.Th("Fecha de admisión"),
+            html.Th("Registrado por"),
+            html.Th("Eliminar"),
+        ]
+    )
+    for record in data:
+        record_cols = [html.Td(str(col)) for col in record]
+        # link_col = html.Td(html.A("Eliminar",id="delete_record_button",className="delete_record_btn",href='/delete_record/%s' % (str(record[0]))))
+        link_col = html.Td(
+            html.A(
+                "X",
+                n_clicks=0,
+                className="delete_record_btn",
+                href="delete_record/%s" % (str(record[0])),
+            )
+        )
+        table_records.append(html.Tr(record_cols + [link_col]))
+    table = html.Table([html.Thead(table_headers), html.Tbody(table_records)], className="table")
+    return table
+
+
+# Delete records modal
+# Medio funciona si agregas los elementos manualmente en el diccionario.
+@app.callback(
+    Output("delete_record_modal", "is_open"),
+    inputs={
+        "all_inputs": {
+            "btn1": Input("delete_record_1", "n_clicks"),
+            "btn2": Input("delete_record_2", "n_clicks"),
+            "btn3": Input("delete_record_3", "n_clicks"),
+        }
+    },
+    prevent_initial_call=True,
+)
+def display(all_inputs):
+    c = ctx.args_grouping.all_inputs
+    if c.btn1.triggered:
+        return True
+    if c.btn2.triggered:
+        return True
+    if c.btn3.triggered:
+        return True
+    return False
+
+
+# Sign out :  When user clicks "close session", the session variable is popped and the user is redirected to login
+@app.callback(
+    Output("url", "pathname"),
+    Input("sign_out_button", "n_clicks"),
+    prevent_initial_call=True,
+)
+def sign_out(clicked):
+    if clicked:
+        session.pop("user", None)
+        return "/login"
+    raise PreventUpdate
+    
+#PASSWORD RECOVERY
+
+#Validate email form input : If the input is empty, the submit button is disabled
+@app.callback(
+    Output("password_recovery_submit_button", "disabled"),
+    Input("recovery_email", "value"))
+
+def password_recovery_input_validation(email):
+        if email is None or check_email(email):
+            return True
+        return False
+
+#Recovery email input feedback : If the email is not valid, feedback will be shown to the user
+@app.callback(
+    Output("recovery_email", "invalid"),
+    Input("recovery_email", "value"),
+    prevent_initial_call=True
+)
+
+def show_password_recovery_email_feedback(email):
+    if check_email(email):
+        return True
+    return False
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
