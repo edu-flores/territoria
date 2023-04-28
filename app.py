3f0cc114f0f104028d35067cca8a9e7fcf630173
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-04-27 23:21:53 -0600

    add map bounds to mty

--- app.py
+++ app.py
@@ -128,11 +128,12 @@ map_layout = dict(
         'zoom': 12,
         'center': dict(lat=25.675456439828732, lon=-100.31115409182688)
     },
+    mapbox_bounds={'west': -100.5, 'east': -100, 'south': 25.5, 'north': 26},
     showlegend=False,
     margin={'l': 0, 'r': 0, 'b': 0, 't': 0},
-    modebar=dict(remove=["zoom", "toimage", "pan", "select", "lasso", "zoomin", "zoomout", "autoscale", "reset",
-                         "resetscale", "resetview"]),
-    hoverlabel_bgcolor="#000000"
+    modebar=dict(remove=['zoom', 'toimage', 'pan', 'select', 'lasso', 'zoomin', 'zoomout', 'autoscale', 'reset',
+                         'resetscale', 'resetview']),
+    hoverlabel_bgcolor='#000000'
 )
 
 # Connect to MariaDB Platform
