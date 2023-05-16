689c9d2e085ee16484eb22718ee1cdfccb6281b4
SofiMontoya <129130771+SofiMontoya@users.noreply.github.com>
2023-05-15 20:22:44 -0600

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -48,7 +48,7 @@ layout = dbc.Container([
 
             html.A(
                 dbc.Row(
-                    dbc.Col(html.Img(src='assets/logo.jpeg', height='30px')),
+                    dbc.Col(html.Img(src='assets/logo.png', height='30px')),
                     align='center', className='g-0 nav_margin_top'
                 ), href='/'
             ),
